8ddcc5672918cbe6ee962dedc2b418ed1ffab0b7
Daalb <daalbor@uninort.edu.co>
2020-11-10 12:52:48 -0500

    Corrigiendo errores

--- SyntaxAnalyzer/ejecutar.sh
+++ SyntaxAnalyzer/ejecutar.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo ""
 echo "LABORATORIO 02"
 echo ""
 rm -rf ArchivoSalida.txt LAB02_Albor_Monsalve_Potte.tab.c LAB02_Albor_Monsalve_Potte.tab.h lex.yy.c sera
 bison -dv LAB02_Albor_Monsalve_Potte.y
-flex LAB01_Albor_Monsalve_Potte.l
+flex LAB02_Albor_Monsalve_Potte.l
 
-echo "**********************"
+echo "****************************************************************************************************************"
 echo "Generando ejecutable"
 gcc LAB02_Albor_Monsalve_Potte.tab.c lex.yy.c -o LAB02 -ll
+
+echo ""
 
 echo "*EJECUTANDO*"
 ./LAB02
--- SyntaxAnalyzer/lex.yy.c
+++ SyntaxAnalyzer/lex.yy.c
@@ -512,8 +512,8 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "LAB01_Albor_Monsalve_Potte.l"
-#line 2 "LAB01_Albor_Monsalve_Potte.l"
+#line 1 "LAB02_Albor_Monsalve_Potte.l"
+#line 2 "LAB02_Albor_Monsalve_Potte.l"
 #include <stdio.h>
 #include "LAB02_Albor_Monsalve_Potte.tab.h"
 int cerror=0;
@@ -735,7 +735,7 @@ YY_DECL
 		}
 
 	{
-#line 20 "LAB01_Albor_Monsalve_Potte.l"
+#line 20 "LAB02_Albor_Monsalve_Potte.l"
 
 #line 741 "lex.yy.c"
 
@@ -796,253 +796,253 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 21 "LAB01_Albor_Monsalve_Potte.l"
+#line 21 "LAB02_Albor_Monsalve_Potte.l"
 { return(IF);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 22 "LAB01_Albor_Monsalve_Potte.l"
+#line 22 "LAB02_Albor_Monsalve_Potte.l"
 { return(ELSE); }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 23 "LAB01_Albor_Monsalve_Potte.l"
+#line 23 "LAB02_Albor_Monsalve_Potte.l"
 { return(FOR);}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 24 "LAB01_Albor_Monsalve_Potte.l"
+#line 24 "LAB02_Albor_Monsalve_Potte.l"
 { return(WHILE);}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 25 "LAB01_Albor_Monsalve_Potte.l"
+#line 25 "LAB02_Albor_Monsalve_Potte.l"
 { return(DOUBLE);}
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 26 "LAB01_Albor_Monsalve_Potte.l"
+#line 26 "LAB02_Albor_Monsalve_Potte.l"
 {  return(INT);}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 27 "LAB01_Albor_Monsalve_Potte.l"
+#line 27 "LAB02_Albor_Monsalve_Potte.l"
 { return(STRING);}
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 28 "LAB01_Albor_Monsalve_Potte.l"
+#line 28 "LAB02_Albor_Monsalve_Potte.l"
 {  return(CHAR);}
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 29 "LAB01_Albor_Monsalve_Potte.l"
+#line 29 "LAB02_Albor_Monsalve_Potte.l"
 { return(NEW);}
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 30 "LAB01_Albor_Monsalve_Potte.l"
+#line 30 "LAB02_Albor_Monsalve_Potte.l"
 {  return(PUBLIC);}
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 31 "LAB01_Albor_Monsalve_Potte.l"
+#line 31 "LAB02_Albor_Monsalve_Potte.l"
 {  return(CLASS);}
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 32 "LAB01_Albor_Monsalve_Potte.l"
+#line 32 "LAB02_Albor_Monsalve_Potte.l"
 { return(VOID);}
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 33 "LAB01_Albor_Monsalve_Potte.l"
+#line 33 "LAB02_Albor_Monsalve_Potte.l"
 {  return(STATIC);}
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 35 "LAB01_Albor_Monsalve_Potte.l"
+#line 35 "LAB02_Albor_Monsalve_Potte.l"
 {  return(MULT);}
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 36 "LAB01_Albor_Monsalve_Potte.l"
+#line 36 "LAB02_Albor_Monsalve_Potte.l"
 { return(SUMA);}
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 37 "LAB01_Albor_Monsalve_Potte.l"
+#line 37 "LAB02_Albor_Monsalve_Potte.l"
 { return(RESTA);}
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 38 "LAB01_Albor_Monsalve_Potte.l"
+#line 38 "LAB02_Albor_Monsalve_Potte.l"
 { return(DIV);}
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 39 "LAB01_Albor_Monsalve_Potte.l"
+#line 39 "LAB02_Albor_Monsalve_Potte.l"
 { return(ASIG);}
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 40 "LAB01_Albor_Monsalve_Potte.l"
+#line 40 "LAB02_Albor_Monsalve_Potte.l"
 { return(MOD);}
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 42 "LAB01_Albor_Monsalve_Potte.l"
+#line 42 "LAB02_Albor_Monsalve_Potte.l"
 {  return(MASMAS);}
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 43 "LAB01_Albor_Monsalve_Potte.l"
+#line 43 "LAB02_Albor_Monsalve_Potte.l"
 { return(MENMEN);}
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 44 "LAB01_Albor_Monsalve_Potte.l"
+#line 44 "LAB02_Albor_Monsalve_Potte.l"
 {return(ASIGMAS);}
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 45 "LAB01_Albor_Monsalve_Potte.l"
+#line 45 "LAB02_Albor_Monsalve_Potte.l"
 {  return(ASIGMEN);}
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 46 "LAB01_Albor_Monsalve_Potte.l"
+#line 46 "LAB02_Albor_Monsalve_Potte.l"
 {  return(ASIGMULT);}
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 47 "LAB01_Albor_Monsalve_Potte.l"
+#line 47 "LAB02_Albor_Monsalve_Potte.l"
 {  return(ASIGDIV);}
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 49 "LAB01_Albor_Monsalve_Potte.l"
+#line 49 "LAB02_Albor_Monsalve_Potte.l"
 {  return(LLAVEA);}
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 50 "LAB01_Albor_Monsalve_Potte.l"
+#line 50 "LAB02_Albor_Monsalve_Potte.l"
 {  return(LLAVEC);}
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 51 "LAB01_Albor_Monsalve_Potte.l"
+#line 51 "LAB02_Albor_Monsalve_Potte.l"
 {  return(PARENTA);}
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 52 "LAB01_Albor_Monsalve_Potte.l"
+#line 52 "LAB02_Albor_Monsalve_Potte.l"
 { return(PARENTC);}
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 53 "LAB01_Albor_Monsalve_Potte.l"
+#line 53 "LAB02_Albor_Monsalve_Potte.l"
 {  return(CORCHETEA);}
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 54 "LAB01_Albor_Monsalve_Potte.l"
+#line 54 "LAB02_Albor_Monsalve_Potte.l"
 {  return(CORCHETEC);}
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 56 "LAB01_Albor_Monsalve_Potte.l"
+#line 56 "LAB02_Albor_Monsalve_Potte.l"
 { return(PUNTOCOMA);}
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 58 "LAB01_Albor_Monsalve_Potte.l"
+#line 58 "LAB02_Albor_Monsalve_Potte.l"
 { return(IGUAL);}
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 59 "LAB01_Albor_Monsalve_Potte.l"
+#line 59 "LAB02_Albor_Monsalve_Potte.l"
 { return(MENORIGUAL);}
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 60 "LAB01_Albor_Monsalve_Potte.l"
+#line 60 "LAB02_Albor_Monsalve_Potte.l"
 { return(MAYORIGUAL);}
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 61 "LAB01_Albor_Monsalve_Potte.l"
+#line 61 "LAB02_Albor_Monsalve_Potte.l"
 { return(DIFERENTE);}
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 62 "LAB01_Albor_Monsalve_Potte.l"
+#line 62 "LAB02_Albor_Monsalve_Potte.l"
 { return(MAYOR);}
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 63 "LAB01_Albor_Monsalve_Potte.l"
+#line 63 "LAB02_Albor_Monsalve_Potte.l"
 { return(MENOR);}
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 65 "LAB01_Albor_Monsalve_Potte.l"
+#line 65 "LAB02_Albor_Monsalve_Potte.l"
 { return(ENTERO);}
 	YY_BREAK
 case 40:
 YY_RULE_SETUP
-#line 66 "LAB01_Albor_Monsalve_Potte.l"
+#line 66 "LAB02_Albor_Monsalve_Potte.l"
 { return(CADENA);}
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 69 "LAB01_Albor_Monsalve_Potte.l"
+#line 69 "LAB02_Albor_Monsalve_Potte.l"
 { }
 	YY_BREAK
 case 42:
 YY_RULE_SETUP
-#line 70 "LAB01_Albor_Monsalve_Potte.l"
+#line 70 "LAB02_Albor_Monsalve_Potte.l"
 { return(REAL);}
 	YY_BREAK
 case 43:
 YY_RULE_SETUP
-#line 71 "LAB01_Albor_Monsalve_Potte.l"
+#line 71 "LAB02_Albor_Monsalve_Potte.l"
 {return (',');}
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 74 "LAB01_Albor_Monsalve_Potte.l"
+#line 74 "LAB02_Albor_Monsalve_Potte.l"
 { return(AND);}
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 75 "LAB01_Albor_Monsalve_Potte.l"
+#line 75 "LAB02_Albor_Monsalve_Potte.l"
 { return(OR);}
 	YY_BREAK
 case 46:
 YY_RULE_SETUP
-#line 76 "LAB01_Albor_Monsalve_Potte.l"
+#line 76 "LAB02_Albor_Monsalve_Potte.l"
 { return(NOT);}
 	YY_BREAK
 case 47:
 YY_RULE_SETUP
-#line 77 "LAB01_Albor_Monsalve_Potte.l"
+#line 77 "LAB02_Albor_Monsalve_Potte.l"
 { return(ID);}
 	YY_BREAK
 case 48:
 YY_RULE_SETUP
-#line 78 "LAB01_Albor_Monsalve_Potte.l"
+#line 78 "LAB02_Albor_Monsalve_Potte.l"
 { }
 	YY_BREAK
 case 49:
 /* rule 49 can match eol */
 YY_RULE_SETUP
-#line 79 "LAB01_Albor_Monsalve_Potte.l"
+#line 79 "LAB02_Albor_Monsalve_Potte.l"
 {cerror++;}
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 80 "LAB01_Albor_Monsalve_Potte.l"
+#line 80 "LAB02_Albor_Monsalve_Potte.l"
 ECHO;
 	YY_BREAK
 #line 1049 "lex.yy.c"
@@ -2050,7 +2050,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 80 "LAB01_Albor_Monsalve_Potte.l"
+#line 80 "LAB02_Albor_Monsalve_Potte.l"
 
 
 
